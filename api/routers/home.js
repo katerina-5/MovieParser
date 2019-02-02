f1801a5c75c26af622f4be6778bc8e35cfe1d64c
katerina-5 <pochta13524@gmail.com>
2019-02-03 00:43:27 +0200

    Change name of directory

--- api/routers/home.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/home');
-
-router.get('/', controller.getBase);
-
-module.exports = router;
--- api/routers/index.js
+++
@@ -1,7 +0,0 @@
-const home = require('./home');
-const movies = require('./movies');
-
-module.exports = function(app) {
-  app.use('/', home);
-  app.use('/movies', movies);
-};
