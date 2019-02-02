7377aeecac967f3f5dd6dfffe9986ea2409605c0
katerina-5 <pochta13524@gmail.com>
2019-02-03 00:44:42 +0200

    Modify settings for server

--- api/app.js
+++ api/app.js
@@ -1,20 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+// const http = require('http');
 
 // require('middleware')(app);
-require('routes')(app);
-require('config/mongodb');
+require('api/routes')(app);
+require('api/config/mongodb.js');
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send({ error: err.message });
 });
