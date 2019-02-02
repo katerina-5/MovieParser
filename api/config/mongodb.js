671b91e54d039df5ba41010f59a0c6f5a7fcfd0b
katerina-5 <pochta13524@gmail.com>
2019-02-03 00:46:31 +0200

    Add changes

--- api/config/mongodb.js
+++ api/config/mongodb.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const connection = mongoose.connection;
+
 mongoose.connect(
   'mongodb://admin:admin12345@ds213615.mlab.com:13615/movies-parser',
   { useNewUrlParser: true }
 );
+
 connection.on('connected', () => {
   console.info(`Mongoose connected to`);
 });
