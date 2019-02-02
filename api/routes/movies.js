acec17840f18d05378d3afc4f2f768085a641c7c
katerina-5 <pochta13524@gmail.com>
2019-02-03 00:41:45 +0200

    Change name of directory

---
+++ api/routes/movies.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+// Require controller modules.
+var movie_controller = require('../controllers/movies');
+
+/// MOVIES ROUTES ///
+
+// GET catalog home page.
+router.get('/', movie_controller.home);
+
+// POST request for creating Movie.
+router.post('/create', movie_controller.create_movie);
+
+// PUT request to update Movie.
+router.put('/:id/update', movie_controller.update_movie);
+
+// DELETE request to delete Movie.
+router.delete('/:id/delete', movie_controller.delete_movie);
+
+// GET request for one Movie.
+router.get('/:id', movie_controller.get_movie_detail);
+
+// GET request for list of all Movie items.
+router.get('/all', movie_controller.get_movie_list);
+
+module.exports = router;
