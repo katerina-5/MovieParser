370511bf09638e8399db745e6c59de5c2b840cb1
katerina-5 <pochta13524@gmail.com>
2019-02-10 15:09:31 +0200

    Add middleware to app.js

--- api/app.js
+++ api/app.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
-// require('middleware')(app);
-require('api/routes')(app);
+require('api/middleware')(app);
 require('api/config/mongodb.js');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+require('api/routes')(app);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
