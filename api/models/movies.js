514cb243055d946fcadedb8dd897458ec41e6d8e
katerina-5 <pochta13524@gmail.com>
2019-02-10 20:48:00 +0200

    modify movie model

--- api/models/movies.js
+++ api/models/movies.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const moviesSchema = new Schema({
-  type: {
+  movieType: {
     type: String,
     required: true
   },
@@ -18,34 +18,35 @@ const moviesSchema = new Schema({
     type: String,
     required: true
   },
-  genre: [
-    {
-      type: String,
-      required: true
-    }
-  ],
+  // genre: [
+  //   {
+  //     type: String,
+  //     required: true
+  //   }
+  // ],
   contentRating: String,
-  actor: [
-    {
-      type: String,
-      url: String,
-      name: String
-    }
-  ],
-  director: [
-    {
-      type: String,
-      url: String,
-      name: String
-    }
-  ],
-  creator: [
-    {
-      type: String,
-      url: String,
-      name: String
-    }
-  ],
+  actors: Array,
+  // actor: [
+  //   {
+  //     type: String,
+  //     url: String,
+  //     name: String
+  //   }
+  // ],
+  // director: [
+  //   {
+  //     type: String,
+  //     url: String,
+  //     name: String
+  //   }
+  // ],
+  // creator: [
+  //   {
+  //     type: String,
+  //     url: String,
+  //     name: String
+  //   }
+  // ],
   description: String,
   datePublished: String,
   keywords: String,
