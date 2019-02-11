a1d758a9e831d81fdcadc082354f5416889e5c18
katerina-5 <pochta13524@gmail.com>
2019-02-12 01:33:01 +0200

    Fix middleware

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,33 +1,9 @@
-// BODY-PARSER
-
 const bodyParser = require('body-parser');
-
-module.exports = function (app) {
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-}
-
-// METHOD-OVERRIDE
-
 const methodOverride = require('method-override');
 
-//override with POST having ?_method=DELETE
 module.exports = function (app) {
-    app.use(methodOverride('_method'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    //override with POST having ?_method=DELETE
+    // app.use(methodOverride('_method'));
 }
-
-//
-
-const multer = require('multer');
-
-const path = require('path');
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/images/products/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({ storage: storage }).single("image");
