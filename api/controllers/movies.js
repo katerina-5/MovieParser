74350a94272254cda2477edfa5f705ff4d7e27ac
katerina-5 <pochta13524@gmail.com>
2019-02-15 22:48:41 +0200

    Add changes to movie-controller

--- api/controllers/movies.js
+++ api/controllers/movies.js
@@ -19,7 +19,6 @@ function get_movie_list(req, res, next) {
 
   Movies.find({})
     .then(movies => {
-      // console.log(movies);
       res.send(movies);
     })
     .catch(error => next(error));
@@ -28,9 +27,6 @@ function get_movie_list(req, res, next) {
 // Display detail page for a specific movie.
 function get_movie_detail(req, res, next) {
   console.log('Movie detail');
-
-  // console.log(req.params.id);
-  // console.log(req.body);
 
   Movies.findById(req.params.id)
     .then(movie => {
@@ -43,8 +39,6 @@ function get_movie_detail(req, res, next) {
 function create_movie(req, res, next) {
   console.log('Movie create');
 
-  // console.log(req.body);
-
   Movies.create(req.body)
     .then(movie => {
       res.send(movie);
@@ -55,8 +49,6 @@ function create_movie(req, res, next) {
 // movie delete on DELETE.
 function delete_movie(req, res, next) {
   console.log('Movie delete');
-
-  // console.log(req.params.id);
 
   Movies.findByIdAndDelete(req.params.id)
     .then(movie => {
@@ -69,9 +61,6 @@ function delete_movie(req, res, next) {
 // movie update on PUT.
 function update_movie(req, res, next) {
   console.log('Movie update');
-
-  // console.log(req.params.id);
-  // console.log(req.body);
 
   Movies.findByIdAndUpdate(req.params.id, req.body)
     .then(movie => {
