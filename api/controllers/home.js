ffc78b608324a80f9fc3931e360b0e4789951710
katerina-5 <pochta13524@gmail.com>
2019-02-25 11:28:18 +0200

    Add rendering of page on server

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -3,5 +3,7 @@ module.exports = {
 };
 
 function getBase(req, res, next) {
-  res.send('Hello, world!\nThis is a home page.');
+  // res.send('Hello, world!\nThis is a home page.');
+  // console.log(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 }
