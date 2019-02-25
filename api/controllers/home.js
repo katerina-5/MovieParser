c92b4892699cfddf83606994a89ff454e89f550f
katerina-5 <pochta13524@gmail.com>
2019-02-25 11:35:16 +0200

    Delete unused code

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -4,6 +4,5 @@ module.exports = {
 
 function getBase(req, res, next) {
   // res.send('Hello, world!\nThis is a home page.');
-  // console.log(__dirname + '/index.html');
   res.sendFile(__dirname + '/index.html');
 }
